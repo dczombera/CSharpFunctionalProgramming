d32e982b72ea4971398ce6f7d1f9a89e5821ea8d
Dennis Czombera <milchschaumschluerfer@gmail.com>
2019-07-31 17:57:08 +0200

    Use partial and currying

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace functional_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            Func<string, string> download = url => client.DownloadString(url);
+            Func<string, Func<string>> downloadCurry = download.Curry();
+
+            var data = download.Partial("https://microsoft.com").WithRetry();
+            var data2 = downloadCurry("https://microsoft.com").WithRetry();
+
+            System.Console.WriteLine(data);
+            System.Console.WriteLine(data2);
+        }
+    }
+}
